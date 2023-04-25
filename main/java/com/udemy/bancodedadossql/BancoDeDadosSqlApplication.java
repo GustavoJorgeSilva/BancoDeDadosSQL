2888a1187b131b3dc9d683b765f2fbaead30d0d4
GustaVoJorgeSilva <gustavo.jorge@hotmail.com.br>
2023-04-25 10:38:38 -0300

    Atualizando dados

--- main/java/com/udemy/bancodedadossql/BancoDeDadosSqlApplication.java
+++ main/java/com/udemy/bancodedadossql/BancoDeDadosSqlApplication.java
@@ -15,51 +15,33 @@ public class BancoDeDadosSqlApplication {
     public static void main(String[] args) {
         SpringApplication.run(BancoDeDadosSqlApplication.class, args);
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
         Connection conn = null;
         PreparedStatement preparedStatement = null;
 
         try {
             conn = DB.getConnection();
-//
-//            preparedStatement = conn.prepareStatement(
-//                    "INSERT INTO seller "
-//                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-//                            + "VALUES "
-//                            + "(?,?,?,?,?)",
-//                    Statement.RETURN_GENERATED_KEYS);
-//
-//            preparedStatement.setString(1, "Carl Purple");
-//            preparedStatement.setString(2, "carl@gmail.com");
-//            preparedStatement.setDate(3, new java.sql.Date(sdf.parse("11/11/1994").getTime()));
-//            preparedStatement.setDouble(4, 3000.0);
-//            preparedStatement.setInt(5, 4);
+            preparedStatement = conn.prepareStatement(
+                    "UPDATE seller " +
+                            "SET BaseSalary = BaseSalary + ? " +
+                            "WHERE " +
+                            "(DepartmentId = ?)"
+            );
 
-            preparedStatement = conn.prepareStatement("insert into department (name) values ('D1'), ('D2')",
-            PreparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement.setDouble(1,200.0);
+            preparedStatement.setInt(2,2);
 
             int rowsAffected = preparedStatement.executeUpdate();
 
-            if (rowsAffected > 0) {
-                ResultSet rs = preparedStatement.getGeneratedKeys();
+            System.out.println("Done! Rows Affected: " + rowsAffected);
 
-                while (rs.next()){
-                    int id = rs.getInt(1);
-                    System.out.println("Done! id = " + id);
-                }
-
-            } else {
-                System.out.println("No rows affected");
-            }
-
-
-        } catch (SQLException e) {
+        } catch (SQLException e){
             e.printStackTrace();
-        }  finally {
+        } finally {
             DB.closeStatement(preparedStatement);
             DB.closeConnection();
         }
+
+
 
 
     }
